f919e10416354e1e29e7cac4b906abb270d4b8a0
Rustem <mr.rustem071193@gmail.com>
2022-06-16 00:50:38 +0600

    signIn signOut logic useContext

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,15 +1,30 @@
 import { useNavigate, useLocation } from 'react-router-dom'
+import { useAuth } from '../../hook/useAuth'
 
 export const LoginPage = () => {
     const navigate = useNavigate()
     const location = useLocation()
+    const { signin } = useAuth()
 
     const formPage = location.state?.form?.pathname || "/"
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const user = form.username.value;
+
+        signin(user, () => navigate(formPage, { replace: true }))
+    }
 
     return (
         <div>
             <h1>Login Page</h1>
-            {formPage}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name: <input name="username" />
+                </label>
+                <button type='submit'>Login</button>
+            </form>
         </div>
     )
 }
