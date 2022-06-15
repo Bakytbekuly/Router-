5c1c83e1c5b75e50a2e7ceb4b5613c5059dd33dd
Rustem <mr.rustem071193@gmail.com>
2022-06-16 00:30:35 +0600

    Auth logic useContext

--- src/App.js
+++ src/App.js
@@ -10,37 +10,38 @@ import { Createpage } from './pages/Createpage';
 import { Editpage } from './pages/Editpage';
 import { LoginPage } from './pages/Login/LoginPage';
 import { RequireAuth } from "./HOC/RequireAuth"
+import { AuthProvider } from './HOC/AuthProvider'
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
+        <AuthProvider>
+          <Routes >
+            <Route path='/' element={<Layout />}>
 
-        <Routes >
-          <Route path='/' element={<Layout />}>
-
-            <Route index element={<Homepage />} />
-            <Route path='about' element={<Aboutpage />} />
-            <Route path='about-us' element={<Navigate to="/about" replace />} />
-
-
-            <Route path='posts' element={<Blogpage />} />
-            <Route path='posts/:id' element={<Singlepage />} />
-            <Route path='posts/:id/edit' element={<Editpage />} />
-            <Route path='posts/new' element={
-              <RequireAuth>
-                <Createpage />
-              </RequireAuth>
-            } />
-
-            <Route path='login' element={<LoginPage />} />
+              <Route index element={<Homepage />} />
+              <Route path='about' element={<Aboutpage />} />
+              <Route path='about-us' element={<Navigate to="/about" replace />} />
 
 
-            <Route path='*' element={<Notfoundpage />} />
+              <Route path='posts' element={<Blogpage />} />
+              <Route path='posts/:id' element={<Singlepage />} />
+              <Route path='posts/:id/edit' element={<Editpage />} />
+              <Route path='posts/new' element={
+                <RequireAuth>
+                  <Createpage />
+                </RequireAuth>
+              } />
 
-          </Route>
-        </Routes>
+              <Route path='login' element={<LoginPage />} />
 
+
+              <Route path='*' element={<Notfoundpage />} />
+
+            </Route>
+          </Routes>
+        </AuthProvider>
       </div>
     </BrowserRouter>
 
